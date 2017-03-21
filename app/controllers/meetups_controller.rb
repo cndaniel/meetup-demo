63b23d80fb27d2e2c0f68d5f1a04b96e88794eca
cndaniel <cndaniel@me.com>
2017-03-21 18:20:17 +0100

    implement create and new action for meetups

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -6,4 +6,21 @@ class MeetupsController < ApplicationController
   def show
     @meetup = Meetup.find(params[:id])
   end
+
+  def new
+    @meetup = Meetup.new
+  end
+
+  def create
+    @meetup = Meetup.new(meetup_params)
+    if @meetup.save
+      redirect_to meetups_path, notice: 'meetup up created!'
+    else
+      render :new, alert: 'failed'
+    end
+  end
+  private
+  def meetup_params
+    params.require(:meetup).permit(:title, :description)
+  end
 end
--- spec/controllers/meetups_controller_spec.rb
+++ spec/controllers/meetups_controller_spec.rb
@@ -25,4 +25,36 @@ RSpec.describe MeetupsController, type: :controller do
       expect(response).to render_template('show')
     end
   end
+
+  describe 'get new' do
+    it 'assigns @course' do
+      meetup = build(:meetup)
+      get :new
+      expect(assigns(:meetup)).to be_a_new(Meetup)
+    end
+    it 'renders template' do
+      meetup = build(:meetup)
+
+      get :new
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe 'POST create' do
+    it "creates a new meetup" do
+      meetup = build(:meetup)
+
+      expect do
+        post :create, params: { meetup: attributes_for(:meetup) }
+      end.to change{ Meetup.count }.by(1)
+    end
+
+    it "redirects to meetups_path" do
+      meetup = build(:meetup)
+      post :create, params: { meetup: attributes_for(:meetup)}
+
+      expect(response).to redirect_to meetups_path
+    end
+  end
+
 end
