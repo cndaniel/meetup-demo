a7f5b202ec5c1b65e047a36256707702f37d6b89
cndaniel <cndaniel@me.com>
2017-03-22 21:20:43 +0100

    hard change comment.content type to text

--- db/migrate/20170321193052_create_comments.rb
+++ db/migrate/20170321193052_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.string  :content
+      t.text :content
       t.integer :meetup_id
       t.integer :user_id
       t.timestamps
